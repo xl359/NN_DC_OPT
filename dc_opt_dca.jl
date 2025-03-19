f5c66079724ffa861dc06f27fa611d5b9cdc1af7
Xinwei Liu <liuxw@stanford.edu>
2025-03-19 11:54:06 -0400

    Add files via upload

---
+++ dc_opt_dca.jl
@@ -0,0 +1,246 @@
+using JLD2
+using JuMP, Gurobi
+using LinearAlgebra, Random
+const GRB_ENV = Gurobi.Env()
+
+include("find_rho.jl")
+ρ  = rho_value()
+ρ_factor = 1.5
+ρ = ρ*ρ_factor
+include("aux_fun.jl")
+
+
+# load OPF data (traning set and network params)
+data = JLD2.load("train_data_paper.jld2", "train_data")
+net = data[:net]
+# load neural network
+nn = JLD2.load("nn_trained_paper.jld2", "nn_trained")
+
+# nn size
+n_in, n_out = 3, 3
+n_hl, n_hn = length(nn[:W])-1, size(nn[:W][2],1)
+
+dc_load_scale = 0.9
+
+
+function nn_sos1_ref(d)
+    #= 
+
+        This function takes in a value of datacenter load and compute the solution of the original problem 
+        without the datacenter load constraints. It is equavaliant to passing it through the neural network.
+
+        Arg: 
+
+            d: data center load
+
+        Return:
+
+            :λ̂  : Charge of the datacenter loads
+            :y  : The hidden neuron values
+            :v  : Result of the ReLU function properity
+        =#
+    model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+
+    @variable(model, y[1:n_hn,1:n_hl]>=0)
+    @variable(model, v[1:n_hn,1:n_hl]>=0)
+    @variable(model, λ̂[1:n_out])
+
+    # mixed-integer reformulation of the trained neural network
+    @constraint(model, y[:,1] .== nn[:W][1]*d      .+ nn[:b][1] .+ v[:,1])
+    @constraint(model, y[:,2] .== nn[:W][2]*y[:,1] .+ nn[:b][2] .+ v[:,2])
+    @constraint(model,      λ̂ .== nn[:W][3]*y[:,2] .+ nn[:b][3])
+    for i in 1:n_hn, k in 1:n_hl
+        @constraint(model, [v[i,k],y[i,k]] in SOS1())
+    end
+
+    optimize!(model)
+    return Dict(:λ̂ => JuMP.value.(λ̂), :y => JuMP.value.(y), :v => JuMP.value.(v))
+end
+
+
+# function to normalize and denormalize loads and charges
+# un_normalize load
+_d_(d) = d .*(data[:d_max]-data[:d_min]) .+ data[:d_min]
+# unnormalize charge
+_λ_(λ) = λ .*(data[:λ_max]-data[:λ_min]) .+ data[:λ_min]
+# normalize load
+_d̂_(d) = (d .- data[:d_min])./(data[:d_max]-data[:d_min])
+# normalize charge
+_λ̂_(λ) = (λ .- data[:λ_min])./(data[:λ_max]-data[:λ_min])
+
+Δ =  sum(dc_load_scale*_d̂_(net[:d]))
+# compute the upper limit
+_d̂_lower = 0.8*_d̂_(net[:d])
+_d̂_upper = _d̂_(net[:d])
+
+
+function opt_sos1()
+    #= 
+
+        This function computes the solution of the original problem 
+        without the datacenter load constraints. It is equavaliant to passing it through the neural network.
+
+
+        Return:
+            :y  : The hidden neuron values
+            :v  : Result of the ReLU function properity
+            :d̂_act: computed optimal load
+            :λ̂_act: computed optimal charge
+            :d̂ : normalized optimal load
+            :λ̂ : normalized optimal charge
+            :obj: sum of the output of the nenral network
+        =#
+    model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+
+    @variable(model, y[1:n_hn,1:n_hl]>=0)
+    @variable(model, v[1:n_hn,1:n_hl]>=0)
+    @variable(model, d̂[1:n_in])
+    @variable(model, λ̂[1:n_out])
+
+    @objective(model, Min, ones(n_out)'*λ̂)
+
+    @constraint(model, d̂ .<= _d̂_(net[:d]))
+    @constraint(model, d̂ .>= 0.8*_d̂_(net[:d]))
+    @constraint(model, ones(n_in)'*d̂ == sum(dc_load_scale*_d̂_(net[:d])))
+
+    # mixed-integer reformulation of the trained neural network
+    @constraint(model, y[:,1] .== nn[:W][1]*d̂      .+ nn[:b][1] .+ v[:,1])
+    @constraint(model, y[:,2] .== nn[:W][2]*y[:,1] .+ nn[:b][2] .+ v[:,2])
+    @constraint(model,      λ̂ .== nn[:W][3]*y[:,2] .+ nn[:b][3])
+    for i in 1:n_hn, k in 1:n_hl
+        @constraint(model, [v[i,k],y[i,k]] in SOS1())
+    end
+
+    optimize!(model)
+    return Dict(:y => JuMP.value.(y), :v => JuMP.value.(v), :d̂_act => _d_(JuMP.value.(d̂)), :λ̂_act => _λ_(JuMP.value.(λ̂)), :d̂ => JuMP.value.(d̂), :λ̂ => JuMP.value.(λ̂), :obj => JuMP.objective_value(model))
+end
+
+
+function opt_dca_sub(ỹ,ṽ)
+    #= 
+    This function takes in the initial value of the hidden neurons and the v vector, which 
+    and returns the dca solution of each iteration_number
+
+    Arg: 
+        ỹ:initial neuron value
+        ṽ: initial v value
+    
+    Return
+        :y  : The hidden neuron values
+        :v  : Result of the ReLU function properity
+        :d̂_act: computed optimal load
+        :λ̂_act: computed optimal charge
+        :d̂ : normalized optimal load
+        :λ̂ : normalized optimal charge
+        :obj: sum of the output of the nenral network
+    =#
+        
+    model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+    JuMP.set_silent(model)
+
+    @variable(model, y[1:n_hn,1:n_hl]>=0)
+    @variable(model, v[1:n_hn,1:n_hl]>=0)
+    @variable(model, d̂[1:n_in])
+    @variable(model, λ̂[1:n_out])
+
+    @objective(model, Min, ones(n_out)'*λ̂
+                                + ρ/4*sum((y[:,i] .+ v[:,i])'*(y[:,i] .+ v[:,i]) for i in 1:n_hl)
+                                - ρ/2*sum((ỹ[:,i] .- ṽ[:,i])'*(y[:,i] .- v[:,i]) for i in 1:n_hl))
+
+    @constraint(model, d̂ .<= _d̂_(net[:d]))
+    @constraint(model, d̂ .>= 0.8*_d̂_(net[:d]))
+    @constraint(model, ones(n_in)'*d̂ == sum(dc_load_scale*_d̂_(net[:d])))
+
+    # mixed-integer reformulation of the trained neural network
+    @constraint(model, y[:,1] .== nn[:W][1]*d̂      .+ nn[:b][1] .+ v[:,1])
+    @constraint(model, y[:,2] .== nn[:W][2]*y[:,1] .+ nn[:b][2] .+ v[:,2])
+    @constraint(model,      λ̂ .== nn[:W][3]*y[:,2] .+ nn[:b][3])
+
+    optimize!(model)
+    return Dict(:y => JuMP.value.(y), :v => JuMP.value.(v), :d̂_act => _d_(JuMP.value.(d̂)), :λ̂_act => _λ_(JuMP.value.(λ̂)), :d̂ => JuMP.value.(d̂), :λ̂ => JuMP.value.(λ̂), :obj => JuMP.objective_value(model))
+end
+
+sol_sos1 = opt_sos1()
+
+
+# Helper functions 
+# penality function
+ϕ(y,v) = 1/4*norm(y+v,2)^2 - 1/4*norm(y-v,2)^2
+# total charge computation function
+f(λ̂,y,v) = ones(n_out)'_λ_(λ̂) + ρ*sum(ϕ(y[:,i],v[:,i]) for i in 1:n_hl)
+# complementary condition violatioin computation
+com_cond(y,v) = sum(y[:,i]'v[:,i] for i in 1:n_hl)
+
+function feasible_d(_d̂_lower,_d̂_upper,Δ)
+    #= 
+
+        This function computes a feasible load value such that it satisfies the 
+        box constraint on the load and the summation of the load = Δ
+    
+    Arg: 
+            _d̂_lower: lower bound on load
+            _d̂_upper: upper bound on load
+            Δ       : sum of the load constraint
+
+    Return: 
+            :d  : load of datacenter. We use it as the starting point of DCA computation. 
+            
+    =#
+
+    model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+    JuMP.set_silent(model)
+    # specifying variable
+    @variable(model,d[1:n_in])
+    # specifying constrains
+    @constraint(model, d .>=_d̂_lower)
+    @constraint(model, d .<=  _d̂_upper)
+    @constraint(model, dot(ones(1,length(d)),d) == Δ)
+    optimize!(model)
+    return Dict(:d => value.(d))
+end
+
+d_init = feasible_d(_d̂_lower,_d̂_upper,Δ)
+d_init = d_init[:d]
+
+# compute initial ỹ and ṽ
+# take the third sample to be the initial d value 
+sample = 3
+sol_init = nn_sos1_ref(data[:d][:,sample])
+λ̂ =sol_init[:λ̂]
+ỹ = sol_init[:y]
+ṽ = sol_init[:v]
+d̃ = zeros(net[:D])
+f_aft = 0
+
+# start the DCA algorithm iteration
+ϵ_tol = 1e-5
+dca_charges = []
+sos1 = []
+iteration_number = 0
+for i in 1:100000
+    global λ̂,ỹ,ṽ,iteration_number
+    iteration_number = iteration_number + 1
+    f_pre = f(λ̂,ỹ,ṽ)
+    sol_dca_sub = opt_dca_sub(ỹ,ṽ)
+    ỹ, ṽ, λ̂, d̃ = sol_dca_sub[:y], sol_dca_sub[:v], sol_dca_sub[:λ̂], sol_dca_sub[:d̂_act]
+    f_aft = f(λ̂,ỹ,ṽ)
+    i % 1 == 0 ? println("i: $i ... f: $(f(λ̂,ỹ,ṽ)) ... comp_viol: $(com_cond(ỹ,ṽ)) ... Δf: $(norm(f_aft-f_pre))") : NaN
+    norm(f_aft-f_pre) <= ϵ_tol ? break : NaN 
+    append!(dca_charges,f_aft)
+    append!(sos1,com_cond(ỹ,ṽ))
+end
+
+
+@show f(λ̂,ỹ,ṽ);
+@show sum(sol_sos1[:λ̂_act]);
+plot(dca_charges)
+
+
+data_set = Dict(:ρ => ρ,
+                  :dca_charges => dca_charges,
+                  :ground_truth_charge_final =>sum(sol_sos1[:λ̂_act]),
+                  :dca_charges_final => f(λ̂,ỹ,ṽ),
+                  :sos1_cond => sos1,
+                  :iteration_number => iteration_number
+                    )
+@save "dca_charges.jld2" data_set
---
+++ find_rho.jl
@@ -0,0 +1,402 @@
+using JLD2
+using JuMP, Gurobi
+using LinearAlgebra
+using Plots
+import Test 
+const GRB_ENV = Gurobi.Env()
+
+
+function rho_value(dc_load_scale = 0.9, n_in = 3, n_out = 3)
+    #= 
+
+    This function takes in the loading scale and the input output dimension of the neural network and 
+        return the optimal rho value for DCA computation
+
+    Arg:
+
+            dc_load_scale: loading scale
+            n_in: input dimension
+            n_out: output dimension 
+    Return: 
+            rho value
+
+    =#
+    # load OPF data (traning set and network params)
+    data = JLD2.load("train_data_paper.jld2", "train_data")
+    net = data[:net]
+    # load neural network
+    nn = JLD2.load("nn_trained_paper.jld2", "nn_trained")
+
+    #dc_load_scale = 0.9
+
+    # nn size
+    #n_in, n_out = 3, 3
+    n_hl, n_hn = length(nn[:W])-1, size(nn[:W][2],1)
+
+    # function to normalize and denormalize loads and charges
+    _d_(d) = d .*(data[:d_max]-data[:d_min]) .+ data[:d_min]
+    _λ_(λ) = λ .*(data[:λ_max]-data[:λ_min]) .+ data[:λ_min]
+    _d̂_(d) = (d .- data[:d_min])./(data[:d_max]-data[:d_min])
+    _λ̂_(λ) = (λ .- data[:λ_min])./(data[:λ_max]-data[:λ_min])
+
+
+    Δ =  sum(dc_load_scale*_d̂_(net[:d]))
+    # compute the upper limit
+    _d̂_lower = 0.8*_d̂_(net[:d])
+    _d̂_upper = _d̂_(net[:d])
+
+    function computing_matricies(n_hl, n_hn,nn, Δ, _d̂_lower, _d̂_upper)
+
+        #= this function inherent the neural network parameter computed above
+            and return several matricies to present the formulation in the
+            vectorized formulation
+        Arg: 
+
+            n_hl: number of hidden layers
+            n_hn: number of nidden neurons of each layer
+            nn: neural network
+            Δ: summation constraint on d
+            _d̂_lower: _d̂_upper: box constraints on the data center loads
+
+        Return: 
+            param: a dictionary containing all the necessary matricies
+        =#
+
+
+        W = float.(Matrix(I, n_hl*n_hn, n_hl*n_hn))
+        for i = 2:n_hl
+            W[((i-1)*n_hn+1):i*n_hn, ((i-2)*n_hn+1):(i-1)*n_hn]=-nn[:W][i]
+        end
+        # compute q
+        b = []
+        for i = 1:n_hl
+            b = [b;-nn[:b][i]]
+        end
+
+        # compute f
+        f_  = [Δ;-Δ;_d̂_lower;-_d̂_upper]
+
+        if n_hl > 1
+            param = Dict("c" => [zeros(1,n_hn*(n_hl-1)) ones(n_out)'*nn[:W][n_hl+1]]', 
+
+                        "A" => [ones(n_in)';-ones(n_in)';Matrix(I, n_in, n_in);-Matrix(I, n_in, n_in)],
+
+                        "f" => f_,
+
+                        "V" => [-nn[:W][1];zeros(n_hn*(n_hl-1),n_in)],
+                                
+                        "W" => W,
+
+                        "b" => b) 
+        end
+        return param
+    end
+    param = computing_matricies(n_hl, n_hn,nn, Δ, _d̂_lower, _d̂_upper)
+
+    function feasible_d(_d̂_lower,_d̂_upper,Δ)
+        #= 
+
+            This function computes a feasible load value such that it satisfies the 
+            box constraint on the load and the summation of the load = Δ
+        
+        Arg: 
+                _d̂_lower: lower bound on load
+                _d̂_upper: upper bound on load
+                Δ       : sum of the load constraint
+
+        Return: 
+                :d  : load of datacenter. We use it as the starting point of DCA computation. 
+                
+        =#
+
+        model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+        JuMP.set_silent(model)
+        # specifying variable
+        @variable(model,d[1:n_in])
+        # specifying constrains
+        @constraint(model, d .>=_d̂_lower)
+        @constraint(model, d .<=  _d̂_upper)
+        @constraint(model, dot(ones(1,length(d)),d) == Δ)
+        optimize!(model)
+        return Dict(:d => value.(d))
+    end
+
+    # compute a initialization of load value such that it satisfies the loading constraints
+    d_init = feasible_d(_d̂_lower,_d̂_upper,Δ)
+
+    function nn_sos1_ref_rho(d)
+
+        #= 
+
+        This function takes in a value of datacenter load and compute the solution of the original problem 
+        without the datacenter load constraints. It is equavaliant to passing it through the neural network.
+
+        Arg: 
+
+            d: data center load
+
+        Return:
+
+            :λ̂  : Charge of the datacenter loads
+            :y  : The hidden neuron values
+            :v  : Result of the ReLU function properity
+        =#
+        model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+        JuMP.set_silent(model)
+        @variable(model, y[1:n_hn,1:n_hl]>=0)
+        @variable(model, v[1:n_hn,1:n_hl]>=0)
+        @variable(model, λ̂[1:n_out])
+
+        # mixed-integer reformulation of the trained neural network
+        @constraint(model, y[:,1] .== nn[:W][1]*d      .+ nn[:b][1] .+ v[:,1])
+        @constraint(model, y[:,2] .== nn[:W][2]*y[:,1] .+ nn[:b][2] .+ v[:,2])
+        @constraint(model,      λ̂ .== nn[:W][3]*y[:,2] .+ nn[:b][3])
+        for i in 1:n_hn, k in 1:n_hl
+            @constraint(model, [v[i,k],y[i,k]] in SOS1())
+        end
+
+        optimize!(model)
+        return Dict(:λ̂ => JuMP.value.(λ̂), :y => JuMP.value.(y), :v => JuMP.value.(v))
+    end
+
+    # compute a point with no y = w = 0
+    d̃ = d_init[:d]
+    sol_init = nn_sos1_ref_rho(d̃);
+    ỹ = vec(sol_init[:y])
+    ṽ = vec(sol_init[:v])
+    sumcond = sum(ỹ .== ṽ)
+    if sumcond != 0
+        println("warning")
+    end
+
+    # find the index set:
+    function index_set(ỹ, ṽ)
+
+        #= 
+        
+        This function computes the index sets corresponding to the RxLP solution
+        
+        Arg:
+            ỹ: y coordinate of the feasble point computed using feasible_d
+            ṽ: v coordinate of the feasble point computed using feasible_d
+        Return: 
+            Iy, Iv, I0 index sets for solving RxLP
+        =#
+            
+        Iy = []
+        Iv = []
+        I0 = []
+
+        for i = 1:length(ỹ)
+        # global Iy,Iv,I0
+                
+
+            if ỹ[i] == 0 && ṽ[i] > 0
+                Iy = [Iy;i]
+            end
+            if ỹ[i] > 0 && ṽ[i] == 0
+                Iv = [Iv;i]
+            end
+            if ỹ[i] == 0 && ṽ[i] == 0
+                I0 = [I0;i]
+            end
+        end
+        return Iy, Iv, I0 
+    end
+
+    Iy, Iv, I0 = index_set(ỹ, ṽ)
+
+    function solve_RxLP(param,Iy,Iv) 
+
+        #= 
+
+            This function takes in the matricies and index sets and comput the solution to the relaxed problem 
+            to retrieve the stronly stationary point 
+        
+        Arg: 
+                param as the output of computing_matricies()
+                Iy, Iv: index sets from index_set(ỹ, ṽ)
+
+        Return: 
+                :d  : The load of datacenter
+                :y  : The hidden neuron values
+                :v  : Result of the ReLU function properity
+                :obj: Sum of the final layer output 
+                
+        =#
+        c = param["c"]
+        A = param["A"]
+        f = param["f"]
+        V = param["V"]
+        W = param["W"]
+        b = param["b"]
+        ddim = n_in
+        ydim = length(b)
+        vdim = length(b)
+        model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+        #set_optimizer_attribute(model, "FeasibilityTol", 1e-2)  # Relax feasibility tolerance
+        JuMP.set_silent(model)
+        @variable(model, d[1:ddim])
+        @variable(model, y[1:ydim])
+        @variable(model, v[1:vdim])
+        @objective(model, Min, c'*y)
+        @constraint(model, ineq, A*d .>= f)
+        @constraint(model, eq, V*d .+ W*y .+ b .== v)
+
+        @constraint(model, y[Iy] .== 0 )
+        @constraint(model, v[Iy] .>= 0 )
+
+        @constraint(model, y[Iv] .>= 0 )
+        @constraint(model, v[Iv] .== 0 )
+        optimize!(model)
+        return Dict(:d=>JuMP.value.(d), :y=>JuMP.value.(y), :v=>JuMP.value.(v),:obj=>JuMP.objective_value(model), :status=>termination_status(model))
+    end
+
+    soln_RxLP = solve_RxLP(param,Iy,Iv)
+
+    ȳ = soln_RxLP[:y]
+    v̄ = soln_RxLP[:v]
+
+    function multiplier_precondition(param,soln_RxLP)
+
+        #= 
+
+            This function takes in the parameter of matricies and the solution of RxLP 
+            Return the set to indicate which indicies should be zero. This function serve
+            to avoid the SOS1 constraint in the multiplier computation since which indicies should be zero
+            is known to us
+
+        Arg: 
+                param: the matrix parameter calculated 
+                solnRxLP: solution to RxLP
+        Return: 
+            Iλ: where λ is zero
+            I0λ: where λ is not zero
+            Iμy: where μy is zero
+            Iμv: where μv is zero
+
+        =#
+        ȳ = soln_RxLP[:y]
+        v̄ = soln_RxLP[:v]
+        # primer for ws solutions:
+        λ_con = param["A"]*soln_RxLP[:d] - param["f"]
+        Iλ = [] # this is the set where lambda has to be 0 
+        I0λ = []# this is the set where lambda does not need to be 0 but still positive
+        for i = 1:length(Iλ)
+
+            global Iλ,I0λ
+
+            if λ_con[i] != 0
+                Iλ = [Iλ;i]
+            else
+                I0λ = [I0λ;i]
+            end
+
+        end
+
+
+        Iμy = []# this is the set where μ has to be 0 
+        Iμv = []
+        for i = 1:length(ȳ)
+
+        # global Iμy,Iμv
+
+            if ȳ[i] != 0
+                Iμy = [Iμy;i]
+            end
+
+            if v̄[i] != 0
+                Iμv = [Iμv;i]
+            end
+
+        end
+        return Iλ,I0λ,Iμy,Iμv
+    end
+
+    Iλ,I0λ,Iμy,Iμv = multiplier_precondition(param,soln_RxLP)
+
+    function solve_multipliers(param,Iλ,I0λ,Iμy,Iμv,Iv,Iy) 
+        #= 
+        This function takes in the parameter and the precondition sets and compute the 
+        lagraugian multiplier of the RxLP solution to retrieve rhobar value
+
+        Arg: 
+            param: the matricies computed 
+            Iλ,I0λ,Iμy,Iμv: index output of multiplier_precondition(param,soln_RxLP)
+            Iv,Iy: output of  index_set(ỹ, ṽ)
+
+        Return:
+            A dictionary that contains 
+                μy and μv for rho computation. 
+        =#
+            
+        c = param["c"]
+        A = param["A"]
+        V = param["V"]
+        W = param["W"]
+        b = param["b"]
+        λdim = size(A,1)
+        μydim = length(b)
+        μvdim = size(V,1)
+        model = Model(() -> Gurobi.Optimizer(GRB_ENV))
+        JuMP.set_silent(model)
+        set_optimizer_attribute(model, "FeasibilityTol", 1e-6)
+        set_optimizer_attribute(model, "MIPGap", 0.01)          # Set MIP gap tolerance for mixed integer problems
+        set_optimizer_attribute(model, "TimeLimit", 300)         # Set time limit to 300 seconds
+        set_optimizer_attribute(model, "Threads", 4) 
+        @variable(model, λ[1:λdim])
+        @variable(model, μy[1:μydim])
+        @variable(model, μv[1:μvdim])
+
+
+        @constraint(model, eq1, A'*λ + V'*μv .== zeros(n_in,1))
+        @constraint(model, eq2, W'*μv .+ μy .== c)
+        
+        @constraint(model, conλset, λ[Iλ] .== 0 )
+        @constraint(model, conμyset, μy[Iμy] .== 0 )
+        @constraint(model, conμwset, μv[Iμv] .== 0 )
+
+        @constraint(model, con0λ, λ[I0λ] .>= 0 )
+        @constraint(model, con0μy, μy[Iv] .>= 0 )
+        @constraint(model, con0μw, μv[Iy] .>= 0 )
+        
+        optimize!(model)
+        return Dict(:λ=>JuMP.value.(λ), :μy =>JuMP.value.(μy), :μv=>JuMP.value.(μv),:status=>termination_status(model))
+    end 
+
+
+    solved_multipliers = solve_multipliers(param,Iλ,I0λ,Iμy,Iμv,Iv,Iy) 
+
+    function compute_ρ(solved_multipliers)
+
+        #= 
+
+        This function computes the \rho value from 
+        the solution of the solution of 
+        solve_multipliers(param,Iλ,I0λ,Iμy,Iμv,Iv,Iy) 
+
+        Arg: 
+            solved_multipliers: solution of solve_multipliers(param,Iλ,I0λ,Iμy,Iμv,Iv,Iy) 
+        
+        Return: \rho
+        
+        =#
+
+        μy = solved_multipliers[:μy]
+        μv = solved_multipliers[:μv]
+        ybar = ȳ[ȳ .> 0]
+        μv = μv[ȳ .> 0]
+        vbar = v̄[v̄ .> 0]
+        μy = μy[v̄ .> 0]
+        minρset = [0;-μv./ybar;-μy./vbar]
+        ρ = maximum(minρset)
+        #rhosort = -sort(-minρset)
+        return ρ
+    end
+
+    ρ = compute_ρ(solved_multipliers)
+
+    return ρ
+end
+
+ρ  = rho_value()
